c54c9f485f38c60abcd2acacd389725ce02fa418
CameronDunning <dunningcameron@gmail.com>
2019-08-29 15:58:05 +0000

    Refactored breedfetcher and made testing material

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,20 +4,23 @@ I have read through it and understand it all (and my original code was very simi
 */
 const request = require("request");
 
-const breed = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+const fetchBreedDescription = function(breed, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+  let breedName = breed;
+  request(url, (error, resp, body) => {
+    if (error) {
+      callback("Failed to request details: " + error, null);
+    }
 
-request(url, (error, resp, body) => {
-  if (error) {
-    return console.log("Failed to request details: ", error);
-  }
+    const data = JSON.parse(body);
 
-  const data = JSON.parse(body);
+    const breed = data[0];
+    if (breed) {
+      callback(null, breed.description);
+    } else {
+      callback(`Failed to find breed ${breedName}`, null);
+    }
+  });
+};
 
-  const breed = data[0];
-  if (breed) {
-    console.log(breed.description);
-  } else {
-    console.log(`Failed to find breed ${breed}`);
-  }
-});
+module.exports = { fetchBreedDescription };
