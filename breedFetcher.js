1e99a5d329535e26a158f0dea47ab7ce9ffeb82f
CameronDunning <dunningcameron@gmail.com>
2019-08-29 14:56:32 +0000

    Cat API breed fetcher which returns the description of a given search breed

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,23 @@
+/* 
+The teacher notes were the student module on compass so I have copied the code but 
+I have read through it and understand it all (and my original code was very similar)
+*/
 const request = require("request");
+
+const breed = process.argv[2];
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+
+request(url, (error, resp, body) => {
+  if (error) {
+    return console.log("Failed to request details: ", error);
+  }
+
+  const data = JSON.parse(body);
+
+  const breed = data[0];
+  if (breed) {
+    console.log(breed.description);
+  } else {
+    console.log(`Failed to find breed ${breed}`);
+  }
+});
